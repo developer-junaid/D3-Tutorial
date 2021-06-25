86e171c507f2545c3dc50e7be7bb644baca0ffa4
developer-junaid <qjunaid623@gmail.com>
2021-06-25 07:09:28 +0500

    added dynamic datal

--- index.js
+++ index.js
@@ -1,37 +1,20 @@
-const canvas = d3.select(".canvas");
+// Data
+const data = [
+  {
+    width: 200,
+    height: 100,
+    fill: "green",
+  },
+];
 
-const svg = canvas.append("svg").attr("height", 600).attr("width", 600);
+// SVG
+const svg = d3.select("svg");
 
-const group = svg.append("g").attr("transform", "translate(50, 100)");
+const rect = svg
+  .select("rect")
+  .data(data)
+  .attr("width", (data, index, selection) => data.width)
+  .attr("height", (data) => data.height)
+  .attr("fill", (data) => data.fill);
 
-// Append shapes to svg container
-group
-  .append("rect")
-  .attr("width", 200)
-  .attr("height", 100)
-  .attr("fill", "blue")
-  .attr("x", 20)
-  .attr("y", 20);
-
-group
-  .append("circle")
-  .attr("r", 50)
-  .attr("cx", 300)
-  .attr("cy", 70)
-  .attr("fill", "pink");
-
-group
-  .append("line")
-  .attr("x1", 370)
-  .attr("x2", 400)
-  .attr("y1", 20)
-  .attr("y2", 120)
-  .attr("stroke", "red");
-
-svg
-  .append("text")
-  .attr("x", 20)
-  .attr("y", 200)
-  .attr("fill", "grey")
-  .text("hello, Junaid!")
-  .style("font-family", "arial");
+console.log(rect);
