3b6eb84298b2fd14a549a91ec1189b25ccddc8cb
developer-junaid <qjunaid623@gmail.com>
2021-06-25 07:34:57 +0500

    created shapes according to data

--- index.js
+++ index.js
@@ -1,38 +1,24 @@
-// Data
-const data = [
-  {
-    width: 200,
-    height: 100,
-    fill: "green",
-  },
-  {
-    width: 100,
-    height: 60,
-    fill: "pink",
-  },
-  {
-    width: 50,
-    height: 30,
-    fill: "red",
-  },
-];
-
-// SVG
+// Select svg container
 const svg = d3.select("svg");
 
-// Join data to rects
-const rects = svg.selectAll("rect").data(data);
+// Get data
+d3.json("planets.json").then((data) => {
+  // On success
+  const circs = svg.selectAll("circle").data(data); // Join Data
 
-// add attrs to rects already in the dom
-rects
-  .attr("width", (data, index, selection) => data.width)
-  .attr("height", (data) => data.height)
-  .attr("fill", (data) => data.fill);
+  // Add Attrs to already present circles
+  circs
+    .attr("cy", 200)
+    .attr("cx", (d) => d.distance)
+    .attr("fill", (d) => d.fill)
+    .attr("r", (d) => d.radius);
 
-// Create virtual elements if they aren't there for the data
-rects
-  .enter()
-  .append("rect")
-  .attr("width", (data, index, selection) => data.width)
-  .attr("height", (data) => data.height)
-  .attr("fill", (data) => data.fill);
+  // Add Attrs to enter selection
+  circs
+    .enter()
+    .append("circle")
+    .attr("cy", 200)
+    .attr("cx", (d) => d.distance)
+    .attr("fill", (d) => d.fill)
+    .attr("r", (d) => d.radius);
+});
